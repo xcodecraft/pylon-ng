0e9536cce4d2fc2b9e45630bca43dfe274c96dae
zuowenjian <zuowenjian@ayibang.com>
2015-05-12 23:27:03 +0800

    fix

--- test/pylon/bootstrap.php
+++ test/pylon/bootstrap.php
@@ -5,7 +5,7 @@ require_once "config_test.php" ;
 XSetting::$logMode   = XSetting::LOG_DEBUG_MODE ;
 XSetting::$prjName   = "pylon-ng" ;
 XSetting::$logTag    = "pylon-ng" ;
-XSetting::$runPath   = "/home/zuowenjian/devspace/pylon-ng/run/test/" ;
+XSetting::$runPath   = XSetting::ensureEnv("RUN_PATH") ;
 XSetting::$bootstrap = "pylonstrap.php" ;
 date_default_timezone_set('PRC');
 XPylon::useEnv() ;
